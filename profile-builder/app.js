84ef910126a5cd722cb0188f29eb43a00f880240
Ian Garnett <designpxls@gmail.com>
2021-07-06 08:28:40 -0400

    Add files via upload

--- profile-builder/app.js
+++ profile-builder/app.js
@@ -2,27 +2,31 @@
 
 'use strict'
 
-// sections & div
-const profileContainer = document.querySelector('.profileInfo-container');
-// image
-let profileImage = document.querySelector('.profile-image');
 // Form Inputs
 const form = document.querySelector("form");
 const inputFirstNme = document.querySelector('#fname');
 const inputLastNme = document.querySelector('#lname');
-// Hobbies 
 const hobby1 = document.querySelector('.hobby1');
 const hobby2 = document.querySelector('.hobby2');
 const hobby3 = document.querySelector('.hobby3');
-// Occupation:
 const jobTitle = document.querySelector('.jobTitle');
-// Pets info
 const petName = document.querySelector('.petName');
-// Colors
-const color1 = document.querySelector('.color1');
-const color2 = document.querySelector('.color2');
-// Pick a Number 
+const colorInput = document.querySelector('.colorInput');
 const favNumber = document.querySelector('.numbers');
+
+//Container Divs
+const petContainer = document.querySelector('.petName-Container');
+const lastNameContainer = document.querySelector('.lastName-Container');
+const firstNameContainer = document.querySelector('.firstName-Container');
+const hobby1Container = document.querySelector('.hobby1-Container');
+const hobby2Container = document.querySelector('.hobby2-Container');
+const jobContainer = document.querySelector('.jobTitle-Container');
+const favColorContainer = document.querySelector('.colorContainer');
+const favNumContainer = document.querySelector('.favNumber-Container');
+
+
+
+
 
 
 
@@ -36,7 +40,7 @@ function Person() {
 
   this.jobTitle = jobTitle.value;
 
-  this.favColor1 = color1.value;
+  this.favColor = colorInput.value;
 
   this.favNumber = favNumber.value;
 
@@ -45,19 +49,17 @@ function Person() {
   this.firstInitial = this.firstName[0];
   this.secondInitial = this.lastName[0];
 
-  this.applyStyles = function(element, text, fontSize, rotation) {
-    this.rotation ? rotation : 0;
+  this.applyStyles = function(element, text, fontSize) {
     var elementToPage = element;
 
     elementToPage.innerText = this[text];
     elementToPage.style.fontSize = fontSize;
-    elementToPage.style.position = "absolute";
-    elementToPage.style.top = `${randomNumber(-2, 80)}%`;
-    elementToPage.style.left = `${randomNumber(2, 60)}%`;
-    elementToPage.style.transform = `rotate(${rotation}deg)`;
+    // elementToPage.style.position = "absolute";
+    // elementToPage.style.top = `${randomNumber(-2, 80)}%`;
+    // elementToPage.style.left = `${randomNumber(2, 60)}%`;
 
     
-    return profileContainer.append(element);
+    return elementToPage;
   }
 
 }
@@ -72,33 +74,26 @@ function StyleElement() {
   Person.call(this);
 
 
-  let initial1 = this.applyStyles(document.createElement('h1'), ['firstInitial'], `${randomNumber(5, 12)}em`);
-  let initial2 = this.applyStyles(document.createElement('h1'), ['secondInitial'], `${randomNumber(5, 12)}em`);
-  // this.hobbyToPage1 = this.applyStyles(document.createElement('p'), ['hobby1'], `${randomNumber(5, 12)}em`);
-  // let hobbyToPage2 = this.applyStyles(document.createElement('p'), ['hobby2']);
-  let jobToPage = this.applyStyles(document.createElement('h5'), ['jobTitle'], `${randomNumber(3, 6)}em`);
-  let favColor1 = this.applyStyles(document.createElement('h5'), ['favColor1'], `${randomNumber(5, 12)}em`);
-  let petToPage = this.applyStyles(document.createElement('p'), ['petName'], `${randomNumber(5, 12)}em`);
-  // let numberToPage = this.applyStyles(document.createElement('h3'), ['favNumber']);
+  this.firstInitialStyle = this.applyStyles(document.createElement('h1'), ['firstInitial'], `${randomNumber(5, 12)}em`);
+  this.lastInitialStyle = this.applyStyles(document.createElement('h1'), ['secondInitial'], `${randomNumber(5, 12)}em`);
+  this.hobby1Style = this.applyStyles(document.createElement('p'), ['hobby1'], `${randomNumber(5, 12)}em`);
+  this.hobby2Style = this.applyStyles(document.createElement('p'), ['hobby2']);
+  this.jobStyle = this.applyStyles(document.createElement('h5'), ['jobTitle'], `${randomNumber(3, 6)}em`);
+  this.favColorStyle = this.applyStyles(document.createElement('h5'), ['favColor'], `${randomNumber(5, 12)}em`);
+  this.petNameStyle = this.applyStyles(document.createElement('p'), ['petName'], `${randomNumber(5, 12)}em`);
+  this.favNumStyle = this.applyStyles(document.createElement('h3'), ['favNumber']);
 
-  
+  this.printToPage = function(element, parentContainer) {
+    parentContainer.append(element);
+  }
+
   this.cloneElements = function() {
     for (let i = 0; i < 3; i++) {
-     this.applyStyles(document.createElement('h4'), ['firstName'], `${randomNumber(5, 12)}em`);
-     this.applyStyles(document.createElement('p'), ['hobby1'], `${randomNumber(2, 3)}em`, `${randomNumber(-90, 90)}em`);
-     this.applyStyles(document.createElement('p'), ['hobby2'], `${randomNumber(1, 4)}em`);
-     this.applyStyles(document.createElement('h5'), ['favColor1'], `${randomNumber(5, 12)}em`);
-     this.applyStyles(document.createElement('h6'), ['petName'], `${randomNumber(3, 6)}em`)
+      element[i];
     }
   }
-  // this.cloneElementsv2 = function() {
-  //   for (let i = 0; i < 2; i++) {
-  //     this.applyStyles(document.createElement('h1'), ['firstInitial'], `${randomNumber(5, 12)}em`);
-  //     this.applyStyles(document.createElement('h1'), ['secondInitial'], `${randomNumber(5, 12)}em`);
-  //   }
-  // }
+  
 }
-
 
 function randomNumber(min, max) {
   min = Math.ceil(min);
@@ -119,8 +114,20 @@ function createProfile() {
   */
   let generateStyle = new StyleElement();
 
-  generateStyle.cloneElements()
-  
+  // generateStyle.cloneElements();
+  generateStyle.printToPage(generateStyle.firstInitialStyle, firstNameContainer);
+  generateStyle.printToPage(generateStyle.lastInitialStyle, lastNameContainer);
+
+  generateStyle.printToPage(generateStyle.hobby1Style, hobby1Container);
+  generateStyle.printToPage(generateStyle.hobby2Style, hobby2Container);
+
+  generateStyle.printToPage(generateStyle.jobStyle, jobContainer);
+
+  generateStyle.printToPage(generateStyle.favColorStyle, favColorContainer);
+
+  generateStyle.printToPage(generateStyle.petNameStyle, petContainer);
+
+  generateStyle.printToPage(generateStyle.favNumStyle, favNumContainer);
 };
 
 
